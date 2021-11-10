ec6d77227c788b356026a14afaf9362f78e25b0e
jaime <jaimenestares1012@gmail.com>
2021-11-10 02:18:02 -0500

    mixins

--- applications/administracion/views.py
+++ applications/administracion/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy, reverse
+import datetime
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #################### impotamos el modelo para trabjar con ellas en el template
 
@@ -6,5 +10,20 @@ from django.views.generic import TemplateView, ListView, CreateView
 class prueba(TemplateView):
     template_name='administracion/index.html'
 
-class InicioSesion(TemplateView):
+
+class InicioSesion(LoginRequiredMixin, TemplateView):
     template_name = 'administracion/panel.html'
+    login_url = reverse_lazy('users:user-login')
+
+class FechaMixin(object):
+
+    def get_context_data(self, **kwargs):
+        context = super(FechaMixin, self).get_context_data(**kwargs)
+        context['fecha'] = datetime.datetime.now()
+        return context
+
+
+
+
+
+

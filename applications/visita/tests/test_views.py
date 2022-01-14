af35110948adaf5e82b2127c7cca790832d3bacf
jaime <jaimenestares1012@gmail.com>
2022-01-13 21:03:05 -0500

    test page 2

--- applications/visita/tests/test_views.py
+++ applications/visita/tests/test_views.py
@@ -5,7 +5,13 @@ from django.urls import reverse
 # se hace la creacion de los test
 class TestViews(TestCase):
     # probamos el view de registrar visita
-    def test_should_show_register_page(self):
+    def test_should_show_anadir_page(self):
         response = self.client.get(reverse("add_visita"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "visita/add_visita.html")
+        
+    # probamos el view de ver la lista de visitas  
+    def test_should_show_lista_page(self):
+        response = self.client.get(reverse("lista_visita"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "visita/prueba.html")

c7d096654c28ae4017cdc78ff714f728d52a8844
jaime <jaimenestares1012@gmail.com>
2022-01-14 01:16:20 -0500

    correccion de incidencias

--- applications/mantenimiento/forms.py
+++ applications/mantenimiento/forms.py
@@ -52,10 +52,40 @@ class my_mantenimiento_form(forms.ModelForm):
         # se intenta validar la faecha ingresada
         if fecha_reserva < fecha_actual:
             # se muestra los mendajes de validacion
-
             raise forms.ValidationError("ingrese una fecha correcta de su reserva ")
         # se retorna si es que ha pasado por todas las validaciones
         return fecha_reserva
+    
+    # creamos la funcion para limiar el numero de personas
+    def clean_nro_trabajadores(self):
+        #capturamos el valor ingresado por el usuario
+        nro_personas = self.cleaned_data['nro_trabajadores']
+        # hacemos que solo ingresen más de 1 persona
+        if nro_personas < 1:
+            # mostramos el mmensaje de error
+            raise forms.ValidationError(
+                "ingrese un número correcto de personas ")
+        if nro_personas > 5:
+            # como maximo 5 personas y mostramos el mensaje
+            raise forms.ValidationError(
+                "Ingrese un número inferior de personas, máximo 5 ")
+            # retornamos el valor inggresado si es que pasó por todas las validaciones
+        return nro_personas
+
+    def clean_nro_departamento(self):
+        #capturamos el valor ingresado por el usuario 1
+        nro_departamento = self.cleaned_data['nro_departamento']
+        # hacemos que solo ingresen más de 100 depatamento
+        if nro_departamento < 100:
+            # mostramos el mensaje de error
+            raise forms.ValidationError(
+                "ingrese correctamente su nro de departamento ")
+        if nro_departamento > 190:
+            # como maximo 190 p y mostramos el mensaje
+            raise forms.ValidationError(
+                "Ingrese correctamente el valor ")
+            # retornamos el valor del depa inggresado si es que pasó por todas las validaciones
+        return nro_departamento
 
 # se crea el form externos
 class ExternosForm(forms.ModelForm):
@@ -96,7 +126,21 @@ class ExternosForm(forms.ModelForm):
             # esta dento de un dni invalido
             print("dentro del ")
             raise forms.ValidationError("ingrese un Dni válido ")
-        # paso la validacion
-        print( dni_visita )
+        # se transforma el dni en str 
+        dni_visita = str(dni_visita)
+        # se transforma de str a lis para poder iterarlo
+        palabra = list(dni_visita)
+        # se itera toda la palabra para encontrar caracteres extrañps
+        for a in palabra:
+            # se busca el signo 
+            if a == '-':
+                print("##############################################3 hubo un error")
+                # se muestra el mensaje de errorx
+                raise forms.ValidationError("ingrese un Dni válido ")
+            # paso la validacion
+            print(a)
+        
+        
+        
         # se retorna el dni visira
         return dni_visita

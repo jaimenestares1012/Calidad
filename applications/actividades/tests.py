268f18781762410f1c00951a009d763a6d7ce41e
Jese Rivas <jese.rivas@unmsm.edu.pe>
2022-01-24 00:08:46 -0500

    eliminando test.py

--- applications/actividades/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-# se crearan los casos de prueba
--- applications/mantenimiento/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-# creacion d elos tetst
